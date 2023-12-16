942f144a56bf4a758a5c6cf17d2ba0ccbefc9ff1
Timothy <timothyletsoko3@gmail.com>
2023-12-16 23:48:56 +0200

    Added try-catch to handle exceptions

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -35,35 +35,40 @@ namespace calculator
             MathsOperations mo = new MathsOperations();
 
             int operationChoice, firstNumber, secondNumber;
-
-            Console.WriteLine("-----Welcome to basic calculator-----");
-            Console.Write("Enter your first number:");
-            firstNumber = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter your second number:");
-            secondNumber = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Choose operation(reply with a number)  \n 1. + \n 2. - \n 3. * \n 4. /");
-            operationChoice = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("-------------------------------");
-
-            switch (operationChoice)
+            try
             {
-                case 1:
-                    Console.WriteLine("{0} + {1} = {2} ", firstNumber, secondNumber, mo.add(firstNumber, secondNumber));
-                    break;
-                case 2:
-                    Console.WriteLine("{0} - {1} = {2} ", firstNumber, secondNumber, mo.minus(firstNumber, secondNumber));
-                    break;
-                case 3:
-                    Console.WriteLine("{0} * {1} = {2} ", firstNumber, secondNumber, mo.multiply(firstNumber, secondNumber));
-                    break;
-                case 4:
-                    Console.WriteLine("{0} / {1} = {2} ", firstNumber, secondNumber, mo.divide(firstNumber, secondNumber));
-                    break;
-                default:
-                    Console.WriteLine("Wrong input!");
-                    break;
-            }
+                Console.WriteLine("-----Welcome to basic calculator-----");
+                Console.Write("Enter your first number:");
+                firstNumber = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter your second number:");
+                secondNumber = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Choose operation(reply with a number)  \n 1. + \n 2. - \n 3. * \n 4. /");
+                operationChoice = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("-------------------------------");
 
+                switch (operationChoice)
+                {
+                    case 1:
+                        Console.WriteLine("{0} + {1} = {2} ", firstNumber, secondNumber, mo.add(firstNumber, secondNumber));
+                        break;
+                    case 2:
+                        Console.WriteLine("{0} - {1} = {2} ", firstNumber, secondNumber, mo.minus(firstNumber, secondNumber));
+                        break;
+                    case 3:
+                        Console.WriteLine("{0} * {1} = {2} ", firstNumber, secondNumber, mo.multiply(firstNumber, secondNumber));
+                        break;
+                    case 4:
+                        Console.WriteLine("{0} / {1} = {2} ", firstNumber, secondNumber, mo.divide(firstNumber, secondNumber));
+                        break;
+                    default:
+                        Console.WriteLine("Wrong input!");
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error!!! {0}", e.Message);
+            }
             Console.ReadLine();
         }
     }
